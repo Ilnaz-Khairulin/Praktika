60a4c3fad902c9feb338dec764a31f9cb0d61c59
Ilnaz <72466445@mail.ru>
2022-09-16 18:36:21 +0500

    Zadacha

--- dotnetApp/Program.cs
+++ dotnetApp/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = 3;
+int b= 10;
+
+if(a > b)
+{
+    Console.WriteLine(a);
+}
+else
+{
+    Console.WriteLine(b);
+}
---
+++ dotnetApp/zadacha2/Program.cs
@@ -0,0 +1,8 @@
+﻿int a = 2, b = 25, c = 4;
+int max = a;
+
+if(a>max) max = a;
+if(b>max) max = b;
+if(c>max) max = c;
+
+Console.WriteLine(max);
